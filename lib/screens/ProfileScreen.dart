259b1f79806b5a89a22f575746564bcbed7fad44
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-10-30 06:55:19 +0700

    Profile Header

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -8,22 +8,61 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  // TODO: 1. Deklarasi varaibel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:
-      Stack(
+    return Scaffold(
+      body: Stack(
         children: [
           Container(
-            height: 200, width: double.infinity, color: Colors.deepPurple,
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
           ),
-          Column(
-            children: [
-            ],
-          )
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                // TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(padding: const EdgeInsets.only(top: 200 - 50),
+                  child:                 Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.deepPurple, width: 2),
+                        shape: BoxShape.circle,
+                        ),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('images/placehorder_image.png'),
+                      ),
+                    ),
+                    if(isSignedIn)
+                    IconButton(
+                      onPressed: (){},
+                       icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50],
+                      ),
+                    ),
+                  ],
+                ),
+                  ),
+                )
+                // TODO: 3. Buat bagian ProfileInfor yang berisi info profil
+                // TODO: 4. Buat ProfileActions yang berisi TextButton sign in/out
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
